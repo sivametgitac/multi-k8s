8c478ea971f74abd8921b1b1f462551e3ec7b9bb
sivametgitac <76824794+sivametgitac@users.noreply.github.com>
2021-04-11 00:16:21 -0400

    updated incorrect docker id

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t sivametdockerid/multi-client:latest -t sivametdockerid/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t sivametdockerid/multi-server:latest -t sivametdockerid/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t sivametdockerid/multi-worker:latest -t sivametdockerid/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t sivametdockid/multi-client:latest -t sivametdockid/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sivametdockid/multi-server:latest -t sivametdockid/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sivametdockid/multi-worker:latest -t sivametdockid/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push sivametdockerid/multi-client:latest
-docker push sivametdockerid/multi-server:latest
-docker push sivametdockerid/multi-worker:latest
+docker push sivametdockid/multi-client:latest
+docker push sivametdockid/multi-server:latest
+docker push sivametdockid/multi-worker:latest
 
-docker push sivametdockerid/multi-client:$SHA
-docker push sivametdockerid/multi-server:$SHA
-docker push sivametdockerid/multi-worker:$SHA
+docker push sivametdockid/multi-client:$SHA
+docker push sivametdockid/multi-server:$SHA
+docker push sivametdockid/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=sivametdockerid/multi-server:$SHA
-kubectl set image deployments/client-deployment client=sivametdockerid/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=sivametdockerid/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=sivametdockid/multi-server:$SHA
+kubectl set image deployments/client-deployment client=sivametdockid/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=sivametdockid/multi-worker:$SHA
